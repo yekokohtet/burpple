4ebe7d800ecf98c00366e744bbacefe19bf91dd3
yekokohtet <yekokohtet69@gmail.com>
2018-01-13 14:48:12 +0630

    - set up view pager (POC level)

--- app/src/main/java/com/padcmyanmar/burpple/activities/BurppleFoodPlacesActivity.java
+++ app/src/main/java/com/padcmyanmar/burpple/activities/BurppleFoodPlacesActivity.java
@@ -1,6 +1,7 @@
 package com.padcmyanmar.burpple.activities;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 
 import com.padcmyanmar.burpple.R;
 import com.padcmyanmar.burpple.adapters.BurppleExclusiveAdapter;
+import com.padcmyanmar.burpple.adapters.FoodPlacesPagerAdapter;
 import com.padcmyanmar.burpple.adapters.FoodPromotionAdapter;
 
 import butterknife.BindView;
@@ -21,6 +23,9 @@ public class BurppleFoodPlacesActivity extends AppCompatActivity {
     @BindView(R.id.rv_burpple_guides)
     RecyclerView rvBurppleGuides;
 
+    @BindView(R.id.vp_food_places)
+    ViewPager vpFoodPlaces;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +33,13 @@ public class BurppleFoodPlacesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         ButterKnife.bind(this, this);
+
+        FoodPlacesPagerAdapter foodPlacesPagerAdapter = new FoodPlacesPagerAdapter(getApplicationContext());
+        vpFoodPlaces.setAdapter(foodPlacesPagerAdapter);
+        vpFoodPlaces.setOffscreenPageLimit(foodPlacesPagerAdapter.getCount());
 
         rvPromotions.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false));
         FoodPromotionAdapter foodPromotionAdapter = new FoodPromotionAdapter(getApplicationContext());
---
+++ app/src/main/java/com/padcmyanmar/burpple/adapters/FoodPlacesPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.padcmyanmar.burpple.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.padcmyanmar.burpple.R;
+
+/**
+ * Created by yekokohtet on 1/13/18.
+ */
+
+public class FoodPlacesPagerAdapter extends PagerAdapter {
+
+    private LayoutInflater mLayoutInflater;
+
+    public FoodPlacesPagerAdapter(Context context) {
+        super();
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (View) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.view_item_food_places, container, false);
+        container.addView(itemView);
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
